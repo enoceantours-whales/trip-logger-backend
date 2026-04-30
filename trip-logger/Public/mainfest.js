0a703c4fb13a08986efd72a7fcf1b431b4d5c69a
enoceantours-whales <enoceantours@gmail.com>
2026-04-30 14:15:48 -0700

    Create mainfest.js

---
+++ trip-logger/Public/mainfest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Enocean Tours Trip Logger",
+  "short_name": "Enocean Logger",
+  "description": "Log wildlife sightings and send trip reports",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#0c4a6e",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://enoceantours.com/s/Enocean_Tours_logo-05.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://enoceantours.com/s/Enocean_Tours_logo-05.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
